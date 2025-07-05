cf856c379c778ca8dcb89d1688cc08ef8b019d79
sliitguy <it23259416@my.sliit.lk>
2025-07-05 23:41:22 +0530

    updating portfolio and about for mobile view

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -52,28 +52,28 @@ const Services = () => {
     return(
         <div className="bg-[#101010] min-h-[100vh] py-[80px] w-screen flex items-center justify-center">
             <div className="w-[80%]  flex flex-col ">
-                <p className="text-[40px] services1 bg-gradient-to-t from-[#433D3A] via-[#C6C4C3] to-[#CAC8C6] bg-clip-text text-transparent">Discover the Services Provided</p>
+                <p className="text-[40px] leading-[48px] services1 bg-gradient-to-t from-[#433D3A] via-[#C6C4C3] to-[#CAC8C6] bg-clip-text text-transparent">Discover the Services Provided</p>
 
                 <div className="flex flex-col md:flex-row items-center justify-center space-y-[30px] md:space-y-[0px] md:space-x-[70px] mt-[30px]">
                     <div className=" services2 space-y-[20px] flex flex-col items-center justify-center">
-                        <div className="bg-[#000] w-[400px] ring-[0.5px] ring-[#4d4d4d] h-[115px] rounded-[20px] flex items-center justify-center">
+                        <div className="bg-[#000] w-[300px] sm:w-[400px] ring-[0.5px] ring-[#4d4d4d] h-[90px] rounded-[15px] flex items-center justify-center">
                             <p className="text-[25px]">UI/UX Designing</p>
                         </div>
-                        <div className="bg-[#000] w-[400px] ring-[0.5px] ring-[#4d4d4d] h-[115px] rounded-[20px] flex items-center justify-center">
+                        <div className="bg-[#000] w-[300px] sm:w-[400px] ring-[0.5px] ring-[#4d4d4d] h-[90px] rounded-[15px] flex items-center justify-center">
                             <p className="text-[25px]">Web App Development</p>
                         </div>
-                        <div className="bg-[#000] w-[400px] ring-[0.5px] ring-[#4d4d4d] h-[115px] rounded-[20px] flex items-center justify-center">
+                        <div className="bg-[#000] w-[300px] sm:w-[400px] ring-[0.5px] ring-[#4d4d4d] h-[90px] rounded-[15px] flex items-center justify-center">
                             <p className="text-[21px] text-center leading-[30px] w-[80%]">Custom Software Development</p>
                         </div>
                     </div>
                     <div className=" services3 space-y-[20px] flex flex-col items-center justify-center">
-                        <div className="bg-[#000] w-[400px] ring-[0.5px] ring-[#4d4d4d] h-[115px] rounded-[20px] flex items-center justify-center">
+                        <div className="bg-[#000] w-[300px] sm:w-[400px] ring-[0.5px] ring-[#4d4d4d] h-[90px] rounded-[15px] flex items-center justify-center">
                             <p className="text-[25px]">Mobile App Development</p>
                         </div>
-                        <div className="bg-[#000] w-[400px] ring-[0.5px] ring-[#4d4d4d] h-[115px] rounded-[20px] flex items-center justify-center">
+                        <div className="bg-[#000] w-[300px] sm:w-[400px] ring-[0.5px] ring-[#4d4d4d] h-[90px] rounded-[15px] flex items-center justify-center">
                             <p className="text-[21px] text-center leading-[30px] w-[80%]">Data Analysis & Visualization</p>
                         </div>
-                        <div className="bg-[#000] w-[400px] ring-[0.5px] ring-[#4d4d4d] h-[115px] rounded-[20px] flex items-center justify-center">
+                        <div className="bg-[#000] w-[300px] sm:w-[400px] ring-[0.5px] ring-[#4d4d4d] h-[90px] rounded-[15px] flex items-center justify-center">
                             <p className="text-[21px] text-center leading-[30px] w-[80%]">Machine Learning & AI Model Development</p>
                         </div>
                     </div>
