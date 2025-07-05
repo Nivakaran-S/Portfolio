7eec4f1f8395d3570ae95ddf94b2ed984006ec79
sliitguy <it23259416@my.sliit.lk>
2025-07-05 23:26:47 +0530

    updating portfolio card for mobile view

--- app/components/PortfolioCard.tsx
+++ app/components/PortfolioCard.tsx
@@ -16,8 +16,8 @@ interface PortfolioCardProps {
 
 const PortfolioCard: React.FC<PortfolioCardProps> = ({ text, lang1, lang2, lang3, lang4, lang5, lang6, text2, onClick }) => {
     return (
-        <div className="bg-[#373435] hover:scale-[105%] transition-transform duration-500 text-[15.27px] sm:h-[350px] ring-[1px] ring-[#373435]  sm:w-[500px] cursor-pointer  flex flex-row pl-[10px]  rounded-[20px]" onClick={onClick}>
-            <div className="sm:w-[38.2%] px-[10px] flex items-center justify-center">
+        <div className="bg-[#373435] hover:scale-[105%] transition-transform duration-500 text-[15.27px] sm:h-[350px] ring-[1px] ring-[#373435]  sm:w-[500px] cursor-pointer  flex flex-col sm:flex-row sm:pl-[10px]  rounded-[20px]" onClick={onClick}>
+            <div className="sm:w-[38.2%] px-[10px] hidden sm:flex items-center justify-center">
                 <div className="flex flex-col">
                     
                     
@@ -36,6 +36,21 @@ const PortfolioCard: React.FC<PortfolioCardProps> = ({ text, lang1, lang2, lang3
             <p className="py-[5px] text-[24px] leading-[29px] pl-[10px] pr-[7px] ">{text}</p>
             <p className="text-[14px] leading-[19px] px-[10px]">{text2}</p>
             
+                <div className="sm:w-[38.2%] px-[10px] flex sm:hidden items-center justify-center">
+                    <div className="grid grid-cols-3 gap-[10px] pt-[15px] pb-[10px] flex-col">
+                        
+                        
+                        <p>{lang1}</p>
+    
+                        
+                        <p>{lang2}</p>
+                        <p>{lang3}</p>
+                        <p>{lang4}</p>
+                        <p>{lang5}</p>
+                        <p>{lang6}</p>
+                    </div>
+                
+                </div>
             <div className="flex flex-row items-center justify-center space-x-[10px] pt-[10px]">
                 
                 <div className="bg-[#000] px-[20px] hover:ring-[0.5px] ring-gray-500  py-[8px] rounded-[20px]">
