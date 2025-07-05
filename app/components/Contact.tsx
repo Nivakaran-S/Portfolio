b9e9659701ab9b8f5159a2ead203eb70febf3109
sliitguy <it23259416@my.sliit.lk>
2025-07-06 00:36:06 +0530

    updating portfolio and certificates for mobile view

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -16,12 +16,16 @@ const Contact : React.FC<ContactModelProps> = ({onContactClick}) => {
     return(
         <div className="bg-[#000] h-[100vh] flex items-center justify-center w-screen">
             <div className="w-[75%] flex flex-row items-center justify-center">
-                <div className="w-[60%]">
+                <div className=" sm:w-[60%]">
+                    <div className="w-[100%] flex sm:hidden py-[20px] items-center justify-center">
+                        <Image src={PlayIcon} alt="" height={200} />
+                    </div>
                     <div className=" text-[40px] leading-[44px] md:text-[60px] md:leading-[65px]">
                         <p>Let's Talk About <span className="text-[#96989A]">Your</span> </p>
                         <p className="text-[#96989A]">Next Project</p>
                     </div>
-                    <div className="mt-[10px] w-[75%] text-[15.27px]">
+                    
+                    <div className="mt-[10px] sm:w-[75%] text-[15.27px]">
                         <p>We'd love to hear from you whether it's a 
                             project inquiry, feedback, or just a friendly
                             hello, don't hesitate to reach out: Let's 
@@ -33,7 +37,7 @@ const Contact : React.FC<ContactModelProps> = ({onContactClick}) => {
                         </div>
                     </div>
                 </div>
-                <div className="w-[40%] flex items-center justify-center">
+                <div className="w-[40%] hidden sm:flex items-center justify-center">
                     <Image src={PlayIcon} alt="" height={400} />
                 </div>
             </div>
