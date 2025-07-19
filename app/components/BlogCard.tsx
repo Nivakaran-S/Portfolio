42bf2efea148e2c05b970927245f6accf9eae8b5
sliitguy <it23259416@my.sliit.lk>
2025-07-19 19:54:17 +0530

    Update miniprojects and case studies ui

--- app/components/BlogCard.tsx
+++ app/components/BlogCard.tsx
@@ -1,4 +1,5 @@
-
+import Newspaper from '../images/news.jpeg'
+import Image from 'next/image';
 
 interface BlogCardProps {
     text: string;
@@ -10,7 +11,7 @@ const BlogCard: React.FC<BlogCardProps> = ({ text, text1, onClick }) => {
     return (
         <div className="bg-[#4b4b4d] hover:scale-[107%] transition-transform duration-500 ring-[0.5px] ring-[#4b4b4d] text-[15.27px] cursor-pointer h-[270px] sm:h-[360px] w-[180px] sm:w-[230px] flex flex-col rounded-[10px] sm:rounded-[20px]" onClick={onClick}>
             <div className="sm:h-[61.80%] h-[55%] bg-[#373435] rounded-t-[20px] hidden sm:flex items-center justify-center">
-                <p className="py-[5px] pr-[7px]">{text}</p>
+                <Image className="h-[100%] w-[100%] rounded-t-[20px]" alt="test" src={Newspaper}/>
             </div>
             <div className="  flex flex-col justify-center py-[5px] px-[13px] h-[100%] sm:h-[38.2%] rounded-b-[20px] rounded-t-[20px] sm:rounded-t-[0px]  sm:bg-[#101010]">
                 <p className="text-[20px]">{text1}</p>
--- app/components/CaseStudyCard.tsx
+++ app/components/CaseStudyCard.tsx
@@ -1,4 +1,5 @@
-
+import Image from "next/image";
+import News from '../images/news.jpeg'
 
 interface CaseStudyProps {
     text: string;
@@ -10,9 +11,15 @@ const CaseStudyCard: React.FC<CaseStudyProps> = ({ text, text1, onClick }) => {
     return (
         <div onClick={onClick} className="h-[350px] hover:ring-[1.6px] hover:ring-gray-300 hover:scale-[105%] transition duration-300 ease-in-out    flex flex-row items-center justify-center cursor-pointer ring-[#5F5F62] ring-[1.5px] rounded-[20px] w-[100%] bg-[#373435]" >
             <div className="hidden sm:flex  w-[40%] h-[100%] rounded-l-[20px] ">
-
+                <Image 
+                    src={News} 
+                    alt="Project Image" 
+                    className="w-full h-full object-cover transition-transform duration-500  rounded-l-[20px]" 
+                    width={500}
+                    height={350}
+                />
             </div>
-            <div className=" sm:w-[60%] px-[30px] sm:px-[20px] h-[100%] bg-[#101010] rounded-[20px] sm:rounded-r-[20px] flex flex-col items-center justify-center">
+            <div className=" sm:w-[60%] px-[30px] sm:px-[20px] h-[100%] bg-[#101010] rounded-[0px] sm:rounded-r-[20px] flex flex-col items-center justify-center">
                 <p className="text-[30px] sm:text-[23px]">{text1}</p>
                 <p className="text-[15px] mt-[10px] sm:mt-[0px] leading-[19px] ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi non itaque officia eligendi sed at veniam eos quod tempora  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum beatae ut repellendus maxime!</p>
             </div>
