727740a50eea99363d387d02553e159272d3efd2
sliitguy <it23259416@my.sliit.lk>
2025-08-15 14:01:54 +0530

    updated ui

--- app/components/CaseStudyCard.tsx
+++ app/components/CaseStudyCard.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import News from '../images/news.jpeg'
+import DOMPurify from 'dompurify';
 
 interface CaseStudyProps {
     text: string;
@@ -10,9 +11,12 @@ interface CaseStudyProps {
 
 
 const CaseStudyCard: React.FC<CaseStudyProps> = ({ text, text1, imageUrl, onClick }) => {
+
+    const sanitizedOverview = text ? DOMPurify.sanitize(text) : '';
+
     return (
         <div onClick={onClick} className="h-[350px] hover:ring-[1.6px] hover:ring-gray-300 hover:scale-[105%] transition duration-300 ease-in-out    flex flex-row items-center justify-center cursor-pointer ring-[#5F5F62] ring-[1.5px] rounded-[20px] w-[450px] bg-[#373435]" >
-            <div className="hidden sm:flex  w-[40%] h-[100%] rounded-l-[20px] ">
+            <div className="hidden sm:flex  w-[30%] h-[100%] rounded-l-[20px] ">
                 <Image 
                     src={imageUrl || News} 
                     alt="Project Image" 
@@ -21,9 +25,15 @@ const CaseStudyCard: React.FC<CaseStudyProps> = ({ text, text1, imageUrl, onClic
                     height={350}
                 />
             </div>
-            <div className=" sm:w-[60%] px-[30px] sm:px-[20px] h-[100%] bg-[#101010] rounded-[20px] sm:rounded-[0px] sm:rounded-r-[20px] flex flex-col items-center justify-center">
-                <p className="text-[30px] sm:text-[23px]">{text1}</p>
-                <p className="text-[15px] mt-[10px] sm:mt-[0px] leading-[19px] ">{text}</p>
+            <div className=" sm:w-[70%] px-[30px] sm:px-[20px] h-[100%] bg-[#101010] rounded-[20px] sm:rounded-[0px] sm:rounded-r-[20px] flex flex-col items-center justify-center">
+                <p className="text-[30px] sm:leading-[33px] sm:mb-[10px] sm:text-[30px]">{text1}</p>
+                
+                { 
+                <div
+                    className="text-[15px] mt-[10px] sm:mt-[0px] leading-[19px]"
+                    dangerouslySetInnerHTML={{ __html: sanitizedOverview }}
+                />
+                }
             </div>
         </div>
     );
